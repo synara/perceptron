393ffc4afb8266f62adb8f824a3b86eed00fdc02
Synara Soares <synara-s@hotmail.com>
2018-12-06 00:33:30 -0300

    segunda versao porem com loop

---
+++ Perceptron/Perceptron/Perceptron.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    public class Perceptron
+    {
+        private static int input;
+
+        public static int ativador(double saida)
+        {
+            if (saida >= 0)
+                return 1;
+            else
+                return -1;
+        }
+
+        public static double perceptron(int entrada, double[] pesos)
+        {
+            double saida = 0.0;
+
+            foreach (var item in pesos)
+            {
+                saida += entrada * item;
+            }
+
+            return ativador(saida);
+        }
+
+        public static double[] calcularPeso(int entrada, double[] pesos, double taxa, double erro)
+        {
+            for (int i = 0; i < pesos.Length; i++)
+            {
+                pesos[i] = pesos[i] + taxa * erro * entrada;
+            }
+
+            return pesos;
+        }
+
+        public static void treinar(int[,,] entradas, double taxa, double[] pesos, int bias = 1)
+        {
+           
+            foreach (var item in entradas)
+            {
+                //o erro possivelmente tá aqui
+                input = input + bias;
+
+                while (true)
+                {
+                    var p = perceptron(input, pesos);
+                    double erro = item - p;
+
+                    if (erro == 0)
+                    {
+                        Console.WriteLine("Correto.");
+                        break;
+                    } else
+                    {
+                        pesos = calcularPeso(input, pesos, taxa, erro);
+                        Console.WriteLine("Incorreto.");
+                    }
+                }
+            }
+        }
+    }
+}
--- Perceptron/Perceptron/Program.cs
+++ Perceptron/Perceptron/Program.cs
@@ -10,50 +10,16 @@ namespace Perceptron
     {
         static void Main(string[] args)
         {
-            int[,] entradas = new int[,] { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } };
-            int[] saidas = { 0, 1, 1, 0 };
-
+            int[, ,] entradas = new int[, ,] { { { 0, 0, -1 }, { 0, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } } };
             Random r = new Random();
 
-            double[] pesos = { r.NextDouble(), r.NextDouble(), r.NextDouble() };
+            //double[] pesos = { r.NextDouble(), r.NextDouble(), r.NextDouble() };
+            double[] pesos = { 1, 1, 1 };
 
-            double taxaAprendizagem = 1;
-            double totalErro = 1;
+            double taxaAprendizagem = 0.2d;
 
-            while (totalErro > 0.2)
-            {
-                totalErro = 0;
-                for (int i = 0; i < entradas.Length/2; i++)
-                {
-                    Console.WriteLine(totalErro);
+            Perceptron.treinar(entradas, taxaAprendizagem, pesos);
 
-                    var um = entradas[i, 0];
-                    var dois = entradas[i, 1];
-                    int ds = deltaSaida(entradas[i, 0], entradas[i, 1], pesos);
-
-                    int erro = saidas[i] - ds;
-
-                    pesos[0] += taxaAprendizagem * erro * entradas[i, 0];
-                    pesos[1] += taxaAprendizagem * erro * entradas[i, 1];
-                    pesos[2] += taxaAprendizagem * erro * 1;
-
-                    totalErro += Math.Abs(erro);
-                }
-
-            }
-
-            Console.WriteLine("Resultados:");
-            for (int i = 0; i < entradas.Length/2; i++)
-                Console.WriteLine(deltaSaida(entradas[i, 0], entradas[i, 1], pesos));
-
-            Console.ReadLine();
-
-        }
-
-        private static int deltaSaida(double e1, double e2, double[] pesos)
-        {
-            double soma = e1 * pesos[0] + e2 * pesos[1] + 1 * pesos[2];
-            return (soma >= 0) ? 1 : 0;
         }
     }
 }
